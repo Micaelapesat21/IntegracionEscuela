79286ba45e2e5d00f4481db71c5f30b0b7125f7f
agiacin <anix_g89@hotmail.com>
2021-08-04 20:03:46 -0300

    cambio en obtenerAlumnosPorCurso

--- controllers/escCursoController.js
+++ controllers/escCursoController.js
@@ -92,7 +92,7 @@ let obtenerAlumnos = (req, res) =>
 let obtenerAlumnosPorCurso = (req, res) =>
 {      
     //Curso.findOne( { _id: "6106d999be08ac21d6013843" }, function(err, listalumnos)
-    Curso.findOne( { _id: req.params.id }, function(err, listalumnos) 
+    Curso.findOne( { _id: req.params.idcurso }, function(err, listalumnos) 
     { 
         res.status(200).send(listalumnos.alumnos);
 
